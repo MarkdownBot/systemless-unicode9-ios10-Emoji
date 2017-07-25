72557db584d3202cdbc07581e8ab7f5113729ef8
jplwolters <jean-pierre.wolters@xs4all.nl>
2017-07-25 23:16:56 +0200

    Create run_once.sh

---
+++ common/run_once.sh
@@ -0,0 +1,13 @@
+#!/system/bin/sh
+# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
+# This will make your scripts compatible even if Magisk change its mount point in the future
+MODDIR=${0%/*}
+
+# This script will be executed once during installation
+# It is called inside the `set_permissions` function in `config.sh`
+
+#copy original fonts.xml to the MODDIR.
+cp /dev/magisk/mirror/system/etc/fonts.xml $MODDIR/system/etc
+
+#copy original fallback_fonts.xml to the MODDIR.
+cp /dev/magisk/mirror/system/etc/fallback_fonts.xml $MODDIR/system/etc
