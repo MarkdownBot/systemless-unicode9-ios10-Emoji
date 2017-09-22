c4ad5eaa68e7b5882afc2b720348d9f4b2ff0ecd
jplwolters <jean-pierre.wolters@xs4all.nl>
2017-09-22 11:55:35 +0200

    Update post-fs-data.sh

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -3,10 +3,6 @@
 # This will make your scripts compatible even if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
-#copies the original NotoColorEmoji and renames it to HTC & samsong emoji font replacing dummy file to reduce download size.
-cp $MODDIR/system/fonts/NotoColorEmoji.ttf $MODDIR/system/fonts/hTC_ColorEmoji.ttf
-cp $MODDIR/system/fonts/NotoColorEmoji.ttf $MODDIR/system/fonts/SamsungColorEmoji.ttf
-
 #copy original fonts.xml to the MODDIR overriting the dummy file.
 #cp /dev/magisk/mirror/system/etc/fonts.xml $MODDIR/system/etc
 
@@ -20,10 +16,14 @@ cp $MODDIR/system/fonts/NotoColorEmoji.ttf $MODDIR/system/fonts/SamsungColorEmoj
 #sed -i 's/<file>NotoColorEmoji.ttf<\/file>/\<!-- <file>NotoColorEmoji.ttf<\/file> -->/g' $MODDIR/system/etc/fallback_fonts.xml
 
 #changes the order of the original fonts.xml file
-#sed -i 's/<!-- fallback fonts -->/<!-- fallback fonts edited by Magisk-ios10-Emoji-font module  --><family><font weight="400" style="normal">NotoColorEmoji.ttf<\/font><\/family>/g' $MODDIR/system/etc/fonts.xml
+sed -i 's/<!-- fallback fonts -->/<!-- fallback fonts edited by Magisk-ios10-Emoji-font module  --><family><font weight="400" style="normal">NotoColorEmoji.ttf<\/font><\/family>/g' $MODDIR/system/etc/fonts.xml
 
 #changes the order of the original fallback_fonts.xml file
 #sed -i 's/<familyset>/<!-- fonts.xml edited by Magisk-ios10-Emoji-font module  --><familyset><familyset><family><fileset><file>NotoColorEmoji.ttf<\/file><\/fileset><\/family>/g' $MODDIR/system/etc/fallback_fonts.xml
+
+#copies the original NotoColorEmoji and renames it to HTC & samsong emoji font replacing dummy file to reduce download size.
+cp $MODDIR/system/fonts/NotoColorEmoji.ttf $MODDIR/system/fonts/hTC_ColorEmoji.ttf
+cp $MODDIR/system/fonts/NotoColorEmoji.ttf $MODDIR/system/fonts/SamsungColorEmoji.ttf
 
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
